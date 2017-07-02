bc89dafc701a6d82aa134547db2c1aadb798e418
Jesse Vogel <jessetvogel@Macbook-Jesse.local>
2017-07-03 00:08:17 +0200

    semi-fixed bandpass

--- synthesizer/synthesizer/unitbandpass.cpp
+++ synthesizer/synthesizer/unitbandpass.cpp
@@ -10,8 +10,9 @@
 const int UnitBandpass::maxOrder = 5;
 
 UnitBandpass::UnitBandpass(Controller* controller, int order) : Unit(controller) {
-    // Set type
+    // Set type and store order
     type = "bandpass";
+    this->order = order;
     
     // May or may not be key dependent
     this->keyDependent = false;
@@ -50,7 +51,6 @@ void UnitBandpass::apply(Instrument* instrument) {
         output[x] = filter->apply(input->output[x]);
 }
 
-
 void UnitBandpass::updateFilter() {
     // See: 'Audio Effects - Theory, Implementation and Application' pp. 59--87
     
@@ -71,96 +71,74 @@ void UnitBandpass::updateFilter() {
     double p2Real[p2Length]; // complex poles
     double p2Imag[p2Length];
     
-    // Lowpass part (mostly copied from unitlowpass.cpp)
-    for(int n = 0;n < qLength/2; ++n)
-        q[n] = -1.0;
-    
-    if(pLength/2 == 1) {
-        gain /= 2.0; // (technically, gamma = 0 so 2.0 * cos(gamma) = 2.0)
-        p[0] = 0.0;
+    // Set roots
+    for(int n = 0;n < qLength; ++n) {
+        // Lowpass has roots -1, highpass has roots +1
+        q[n] = n < qLength/2 ? -1.0 : 1.0;
     }
     
-    double gamma;
-    for(int n = 0;n < p2Length/2; ++n) {
-        gamma = M_PI * 0.25 * ((2.0 * n + 1.0) / order - 1.0);
-        double c = cos(gamma);
-        gain /= 4.0 * c * c;
-        p2Real[n] = 0.0;
-        p2Imag[n] = tan(gamma);
+    // Set poles
+    if(pLength == 2) {
+        gain /= 4.0;
+        p[0] = p[1] = 0.0;
     }
     
-    // Change cut-off frequency
-    for(int n = 0;n < qLength/2; ++n) {
-        // Update roots
-        gain *= (1.0 + betaH * q[n]); // Note: for the lowpass filter, the higher cut-off frequency must be used
-        q[n] = (q[n] + betaH) / (1.0 + betaH * q[n]);
+    double gamma, c;
+    for(int n = 0;n < p2Length; ++n) {
+        if(n < p2Length/2) {
+            // Lowpass part
+            gamma = M_PI * 0.25 * ((2.0 * n + 1.0) / order - 1.0);
+            c = cos(gamma);
+            gain /= 4.0 * c * c;
+            p2Real[n] = 0.0;
+            p2Imag[n] = tan(gamma);
+        }
+        else {
+            // Highpass part
+            gamma = M_PI * 0.25 * ((2.0 * (n - p2Length/2) + 1.0) / order - 1.0);
+            c = cos(gamma);
+            gain /= 4.0 * c * c;
+            p2Real[n] = 0.0;
+            p2Imag[n] = tan(gamma);
+        }
     }
     
-    for(int n = 0;n < pLength/2; ++n) {
-        // Update poles (real)
-        gain /= (1.0 + betaH * p[n]);
-        p[n] = (p[n] + betaH) / (1.0 + betaH * p[n]);
+    // Change cut-off frequency:
+    
+    // Update roots
+    double beta;
+    for(int n = 0;n < qLength; ++n) {
+        // For lowpass filter, we use the higher cut-off frequency, for the highpass we use the lower cut-off frequency
+        beta = n < qLength/2 ? betaH : betaL;
+        gain *= (1.0 + beta * q[n]);
+        q[n] = (q[n] + beta) / (1.0 + beta * q[n]);
     }
     
-    for(int n = 0;n < p2Length/2; ++n) {
-        // Update poles (complex)
+    // Update poles (real)
+    for(int n = 0;n < pLength; ++n) {
+        beta = n < pLength/2 ? betaH : betaL;
+        gain /= (1.0 + beta * p[n]);
+        p[n] = (p[n] + beta) / (1.0 + beta * p[n]);
+    }
+
+    // Update poles (complex)
+    for(int n = 0;n < p2Length; ++n) {
+        beta = n < p2Length/2 ? betaH : betaL;
         double a = p2Real[n];
         double b = p2Imag[n];
-        double tmp = 1.0 + betaH * a; tmp *= tmp; tmp += betaH * betaH * b * b;
+        double tmp = 1.0 + beta * a; tmp *= tmp; tmp += beta * beta * b * b;
         gain /= tmp;
-        p2Real[n] = ((a + betaH) * (1.0 + betaH * a) + betaH * b * b) / tmp;
-        p2Imag[n] = ((1.0 + betaH * a) * b - betaH * b * (a + betaH)) / tmp;
-    }
-    
-    // Highpass part
-    for(int n = 0;n < qLength; ++n)
-        q[qLength/2 + n] = -1.0;
-    
-    if(pLength/2 == 1) {
-        gain /= 2.0; // (technically, gamma = 0 so 2.0 * cos(gamma) = 2.0)
-        p[pLength/2 + 0] = 0.0;
-    }
-    
-    for(int n = 0;n < p2Length/2; ++n) {
-        gamma = M_PI * 0.25 * ((2.0 * n + 1.0) / order - 1.0);
-        double c = cos(gamma);
-        gain /= 4.0 * c * c;
-        p2Real[p2Length/2 + n] = 0.0;
-        p2Imag[p2Length/2 + n] = tan(gamma);
-    }
-    
-    // For a highpass filter: change z into -z, i.e. all roots and poles will be made negative
-    for(int n = 0;n < qLength/2; ++n)
-        q[qLength/2 + n] = -q[qLength/2 + n];
-    
-    // Change cut-off frequency
-    for(int n = 0;n < qLength/2; ++n) {
-        // Update roots
-        gain *= (1.0 + betaL * q[qLength/2 + n]); // Note: for the highpass filter, the lower cut-off frequency must be used
-        q[qLength/2 + n] = (q[qLength/2 + n] + betaL) / (1.0 + betaL * q[qLength/2 + n]);
-    }
-    
-    for(int n = 0;n < pLength/2; ++n) {
-        // Update poles (real)
-        gain /= (1.0 + betaL * p[pLength/2 + n]);
-        p[pLength/2 + n] = (p[pLength/2 + n] + betaL) / (1.0 + betaL * p[pLength/2 + n]);
-    }
-    
-    for(int n = 0;n < p2Length/2; ++n) {
-        // Update poles (complex)
-        double a = p2Real[p2Length/2 + n];
-        double b = p2Imag[p2Length/2 + n];
-        double tmp = 1.0 + betaL * a; tmp *= tmp; tmp += betaL * betaL * b * b;
-        gain /= tmp;
-        p2Real[p2Length/2 + n] = ((a + betaL) * (1.0 + betaL * a) + betaL * b * b) / tmp;
-        p2Imag[p2Length/2 + n] = ((1.0 + betaL * a) * b - betaL * b * (a + betaL)) / tmp;
+        p2Real[n] = ((a + beta) * (1.0 + beta * a) + beta * b * b) / tmp;
+        p2Imag[n] = ((1.0 + beta * a) * b - beta * b * (a + beta)) / tmp;
     }
     
     // Now compute polynomial coefficients from poles and roots
-    double a[qLength + 1];
-    double b[pLength + 2 * p2Length + 1];
-    for(int n = 0;n <= order; ++n)
-        a[n] = b[n] = 0.0;
+    int orderX = qLength;
+    int orderY = pLength + 2 * p2Length;
+    double a[orderX + 1];
+    double b[orderY + 1];
+    memset(a, 0, sizeof(double) * (orderX + 1));
+    memset(b, 0, sizeof(double) * (orderY + 1));
     
     // Polynomial of X(z)
     b[0] = 1.0;
@@ -193,10 +171,10 @@ void UnitBandpass::updateFilter() {
     }
     
     // Dividing by z^n translates into 'reversing the coefficients'
-    for(int n = 0;n <= order; ++n) {
-        filter->alpha[n] = a[order - n];
-        filter->beta[n] = b[order - n];
-    }
+    for(int n = 0;n < orderX + 1; ++n)
+        filter->alpha[n] = a[orderX - n];
+    for(int n = 0;n < orderY + 1; ++n)
+        filter->beta[n] = b[orderY - n];
     
     filter->gain = gain;
 }
